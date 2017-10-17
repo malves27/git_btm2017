3f2d9bf04717b09c4a295eee5cdbb7021da24e69
malves27 <mariana.alves@embl.de>
2017-10-17 09:11:52 +0100

    added transaction function

--- file_manager.py
+++ file_manager.py
@@ -9,10 +9,16 @@ def add_client(client_name):
     F.close()
       
 def add_transaction(debtor, creditor, amount):
-    pass
+    F = open(transaction_filename, "w")
+    F.write('\t'.join([debtor, creditor, amount]))
+    F.close()
         
 def get_clients():
     pass
 
 def get_transactions():
     pass
+    
+
+
+
