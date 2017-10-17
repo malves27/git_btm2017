8d8ddfdda644c21a0b96e629d398e65882217222
predoc <predoc@BTM-debian.BTM-debian>
2017-10-17 08:50:09 +0100

    start client function

--- file_manager.py
+++ file_manager.py
@@ -4,7 +4,9 @@ client_filename = "clients.txt"
 transaction_filename = "transactions.txt"
 
 def add_client(client_name):
-    pass
+    F = open(client_filename,"w")
+    F.write(client_name)
+    F.close()
       
 def add_transaction(debtor, creditor, amount):
     pass
